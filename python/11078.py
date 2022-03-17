fc98b3d3a6873500aa271486618fe9abb3753ac4
The Peeps191 <thepeeps.wang@gmail.com>
2022-03-17 16:55:05 -0600

    add porblems

--- python/11078.py
+++ python/11078.py
@@ -1,3 +1,9 @@
 for _ in range(int(input())):
 	s = int(input())
-	print()
+	scores = [int(input()) for _ in range(s)]
+	max_diff = -999999999999999999999999
+	cv = scores[0];
+	for i in range(1, len(scores)):
+		max_diff = max(max_diff, cv - scores[i])
+		cv = max(cv, scores[i])
+	print(max_diff)
