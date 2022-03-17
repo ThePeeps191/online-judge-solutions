846cc299ce71c7a6c46c6f62f96b9c7c6feebb96
The Peeps191 <thepeeps.wang@gmail.com>
2022-03-17 16:39:10 -0600

    add problems

--- python/12015.py
+++ python/12015.py
@@ -1,1 +1,7 @@
-T = int(input())
+for i in range(int(input())):
+	urls = [input().split() for _ in range(10)]
+	rating = max([int(a[1]) for a in urls])
+	print(f"Case #{i + 1}:")
+	for u in urls:
+		if int(u[1]) == rating:
+			print(u[0])
