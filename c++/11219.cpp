fba0299965d712af4ec8f52dd08c767bb4d4b120
Danny Wang <wang.da@wcs-g.com>
2022-07-08 19:22:36 -0600

    add problems

---
+++ c++/11219.cpp
@@ -0,0 +1,15 @@
+// not finished////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin << T;
+    string empty;
+    for(int i = 1; i <= T; i++) {
+        cin >> empty;
+
+    }
+ }
