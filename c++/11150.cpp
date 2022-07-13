8c359f8db79287addae66db4f88ef28cdd9b11db
Danny Wang <wang.da@wcs-g.com>
2022-07-12 20:12:07 -0600

    add some

--- c++/11150.cpp
+++ c++/11150.cpp
@@ -3,14 +3,8 @@
 using namespace std;
 
 int main() {
-    int num;
-    cin >> num;
-    int sum = num;
-    int num1 = 0;
-    while (num >= 3) {
-        int p = num / 3;
-        sum += p;
-        int t = num % 3;
-        num 
+    int N;
+    while(cin >> N) {
+        
     }
 }
---
+++ c++/11185.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void process(int x) {
+    while(x > )) {
+        cout << x % 3;
+    }
+}
+
+int main() {
+    while (true) {
+        int N;
+        cin >> N;
+        if(N < 0) break;
+        process(N);
+    }
+}
