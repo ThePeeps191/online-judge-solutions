e912448b6ebf1afa05f4bf3f84c9001637564d86
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-07-15 18:49:49 -0600

    add problems

--- c++/10286.cpp
+++ c++/10286.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
-
+const double pi = 4 * atan(1);
 int main() {
     double N;
     while(cin >> N) {
-        
+        double x = sin(108 * pi / 180) / sin(63 * pi / 180) * N;
+        cout << fixed << setprecision(10) << x << endl;
     }
 }
