35b372487757dfc562e7ef8771cc2685952905b1
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-07-15 19:45:48 -0600

    add problems

--- c++/11185.cpp
+++ c++/11185.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void process(int x) {
-    while(x > )) {
-        cout << x % 3;
-    }
+  vector<int> v;
+  while(x > 0) {
+    v.push_back(x % 3);
+    x = x / 3;
+  }
+
+  for(int i = v.size() - 1; i >= 0; i--) {
+    cout << v[i];
+  }
+  cout << endl;
 }
 
 int main() {
-    while (true) {
-        int N;
-        cin >> N;
-        if(N < 0) break;
-        process(N);
-    }
+  while (true) {
+    int N;
+    cin >> N;
+    if(N < 0) break;
+    process(N);
+  }
 }
