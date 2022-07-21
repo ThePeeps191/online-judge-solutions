7d08de0086ebae8578a389e3add38f9a3ce5aaa0
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-07-21 13:31:59 -0600

    add

---
+++ c++/10474.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, Q;
+    while(cin >> N >> Q) {
+        
+    }
+}
---
+++ c++/1339.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+string perm_ciph(string s) {
+    
+}
+
+string subs_ciph(string s) {
+    
+}
+
+int main() {
+    string a, b;
+    while(cin >> a) {
+        cin >> b;
+    }
+}
