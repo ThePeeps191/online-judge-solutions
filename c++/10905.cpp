08483ff4cc2de4fac2651b152e2e7627131bf157
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2022-07-22 09:41:44 -0600

    e

--- c++/10905.cpp
+++ c++/10905.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    
+    int N;
+    while(true) {
+        cin >> N;
+        if (N == 0) {
+            break;
+        }
+        vector<string> v;
+        for(int i = 0; i < N; i++) {
+            string x;
+            cin >> x;
+            v.push_back(x);
+        }
+        sort(v.begin(), v.end(), [](string &s1, &s2))
+    }
 }
